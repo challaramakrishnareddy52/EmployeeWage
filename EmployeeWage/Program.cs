399113c9acfd3fccd782f709b9997be8f34fc60b
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-10 13:25:17 +0530

    Add part time employee

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,13 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome");
-            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0,3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
                 empHrs = 8;
