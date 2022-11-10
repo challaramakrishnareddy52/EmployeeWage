b733bffe49f080e5a359033b0206bbd7097e86c1
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-10 12:03:45 +0530

    EmployeeAttendenceUC1

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace EmployeeWage
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome");
+            const int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
