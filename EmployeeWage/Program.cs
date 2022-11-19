21ac4d0c2cba3069f8578d0edc60f31c6afb425f
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-19 22:23:41 +0530

    [200~Refactor the Code

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,41 +2,52 @@
 {
     class Program
     {
-        static void Main(string[] args)
+     
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;    
+        public const int NUM_OF_WORKING_DAYS = 4;     
+        public const int MAX_HRS_IN_MONTH = 16;        
+        public static int ComputeEmpWage()      
         {
-            Console.WriteLine("Welcome");
-            const int IS_PART_TIME = 1;
-            const int IS_FULL_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-            const int NUM_OF_WORKING_DAYS = 20;
-            const int MAX_HRS_IN_MONTH = 100;
-            int empHrs = 0,totalEmpWage =0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            int empHrs;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)   
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
                     case IS_PART_TIME:
                         empHrs = 4;
                         break;
-                    case IS_FULL_TIME:
-
-                        Console.WriteLine("Employee is present");
-                        empHrs = 8;
-                        break;
                     default:
-                        Console.WriteLine("Employee is absent");
                         empHrs = 0;
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+                if (totalEmpHrs > MAX_HRS_IN_MONTH) 
+                {
+                    totalEmpHrs = MAX_HRS_IN_MONTH;
+                }
+                Console.WriteLine("Day#:" + totalWorkingDays + "  " + "Working Hours : " + empHrs);
             }
-            int totalempWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;     
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
         }
+        static void Main(string[] args)
+        {
+            ComputeEmpWage();
         }
     }
+}
 
-    
+
+
+
