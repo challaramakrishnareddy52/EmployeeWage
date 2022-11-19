25edd502f152c00d211d425ecebd540b9c23d69a
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-19 22:35:03 +0530

    Compute Employee Wage for Multiple Companies

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,48 +2,43 @@
 {
     class Program
     {
-     
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;    
-        public const int NUM_OF_WORKING_DAYS = 4;     
-        public const int MAX_HRS_IN_MONTH = 16;        
-        public static int ComputeEmpWage()      
+        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            int empHrs;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)   
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;      
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)       
             {
                 totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
+                Random random = new();
+                int empCheck = random.Next(3);
                 switch (empCheck)
                 {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
                     case IS_PART_TIME:
                         empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
                 totalEmpHrs += empHrs;
-                if (totalEmpHrs > MAX_HRS_IN_MONTH) 
+                if (totalEmpHrs > maxHoursPerMonth)  
                 {
-                    totalEmpHrs = MAX_HRS_IN_MONTH;
+                    totalEmpHrs = maxHoursPerMonth;
                 }
-                Console.WriteLine("Day#:" + totalWorkingDays + "  " + "Working Hours : " + empHrs);
+                
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;     
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs; 
+            Console.WriteLine($"Total Emp Wage for the Company \"{company}\" is : {totalEmpWage}");
             return totalEmpWage;
         }
         static void Main(string[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("Dmart", 20, 3, 10);
+            ComputeEmpWage("Reliance", 25, 4, 20);
         }
     }
 }
