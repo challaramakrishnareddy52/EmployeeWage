112bdb83d586f32be85da3aca821348a598ef5d5
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-25 19:26:51 +0530

    Replaced Array with ArrayList

--- EmployeeWage/EmpWageBuilderArray.cs
+++ EmployeeWage/EmpWageBuilderArray.cs
@@ -1,26 +1,27 @@
-﻿namespace EmployeeWage
+﻿using EmployeeWage;
+using EmployeeWageDemo;
+using System.Collections;
+
+namespace EmployeeWage
 {
-    internal class EmpWageBuilderArray
+    public class EmpWageBuilderArray : IComputeEmpWage
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        private int numOfCompany = 0;   
-        private CompanyEmpWage[] companyEmpWageArray;
-        public EmpWageBuilderArray()
-        {
-            this.companyEmpWageArray = new CompanyEmpWage[5];
-        }
+        ArrayList listObj = new ArrayList();    //using ArrayList instead of Array.
+        private CompanyEmpWage companyEmpWageObj;
         public void AddCompanyEmpWage(string company, int empRateperHour, int numOfWorkingDays, int maxHoursPermonth)
         {
-            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRateperHour, numOfWorkingDays, maxHoursPermonth);
-            numOfCompany++; 
+            this.companyEmpWageObj = new CompanyEmpWage(company, empRateperHour, numOfWorkingDays, maxHoursPermonth);
+            listObj.Add(this.companyEmpWageObj);
         }
         public void ComputeEmpWage()
         {
-            for (int i = 0; i < numOfCompany; i++)
+            foreach (CompanyEmpWage companyEmpWage in listObj)
             {
-                companyEmpWageArray[i].setTotalEmpWage(this.ComputeEmpWage(this.companyEmpWageArray[i]));
-                Console.WriteLine(this.companyEmpWageArray[i].ToString());
+                companyEmpWageObj.SetTotalEmpWage(ComputeEmpWage(companyEmpWage));
+                companyEmpWage.SetTotalEmpWage(companyEmpWageObj.totalEmpWage);
+                Console.WriteLine(companyEmpWage.ToString());
             }
         }
         private int ComputeEmpWage(CompanyEmpWage companyEmpWage)
